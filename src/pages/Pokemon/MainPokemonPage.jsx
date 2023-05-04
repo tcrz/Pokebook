1e966b085e20c9b1bf2319f9108f90ed7c3cdbc3
“tcrz” <tonybaidoo0@gmail.com>
2023-05-04 22:10:53 +0000

    pokemon page updates

--- src/pages/Pokemon/MainPokemonPage.jsx
+++ src/pages/Pokemon/MainPokemonPage.jsx
@@ -27,7 +27,36 @@ const MainPokemonPage = () => {
             </div>
             
         </nav>
-        <main>
+        <main className="borderr-2 border-black" style={{height: "91vh"}}>
+            <section className="borderr border-red-400" style={{height: "78vh"}}>
+                <div className="pokemon-list grid grid-cols-4 gap-x-4 gap-y-4 px-20 py-10 h-full overflow-y-scroll">
+                    {[...Array(10).keys()].map(i => (
+                        <article className="pokemon bgg-red-400 borderr-4 border-orange-500 flex flex-col items-center relative" style={{height: "280pxx"}}>
+                            {/* <div className="absolute border border-pink-950" style={{top: "0%", left:"1%"}}> */}
+                                <img className="absolute top-0" src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"} style={{width: "75%", height: "75%", borrder: "1px solid red", top: "-5%"}}/>
+                            {/* </div> */}
+                            <div className="inner-card border border-white w-full h-full p-2 flex flex-col gap-6" style={{marginTop: "18%"}}>
+                                <div className="inner-card-bg borderr border-black w-full flex flex-col items-center" style={{marginToop: "25%", height: "60%"}}>
+                                    {/* <p>Pokemon</p> */}
+
+                                    
+                                    
+                                </div>
+                                <div className="borderr border-blue-900 flex flex-col items-center">
+                                    <h2 className="text-xl font-medium">Charizard</h2>
+                                    <div>
+                                        <p>skills</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    ))}
+                </div>
+
+            </section>
+            <div className="pagination borderr border-green-400" style={{height: "12vh"}}>
+
+            </div>
 
         </main>
         {/* MainPokemonPage */}
