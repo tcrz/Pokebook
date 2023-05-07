db69585e6a4d3869e152d16debdbbea0e89f0414
“tcrz” <tonybaidoo0@gmail.com>
2023-05-07 22:11:47 +0000

    add navigation to search bar input

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { InputAdornment, OutlinedInput } from '@mui/material'
 import pokemonGroup from './pokemongrp.png'
 import "./Homepage.css"
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai';
 
 const Homepage = () => {
+  const navigate = useNavigate()
   return (
     <div className="hero-bg bg-noiseBg h-screen flex flex-col items-center justify-aroundd borderr-2 border-black">
         <div className="hero h-full gap-6 borderr-4 border-green-400 flex flex-col items-center">
@@ -15,11 +16,10 @@ const Homepage = () => {
                 <p style={{width: "70%"}}>Largest Pokémon index with information about every Pokemon you can think of.</p>
             </div>
             <div className="borderr text-center w-full">
-              <OutlinedInput className="input-field hero-input-field mb-3" style={{width: "75%"}} type="text" placeholder="Enter pokemon name"
+              <OutlinedInput onClick={()=>navigate("/all")} className="cursor-pointer input-field hero-input-field mb-3" style={{width: "75%"}} type="text" placeholder="Enter pokemon name"
               endAdornment={<InputAdornment position="end"><p className='text-sm text-white rounded-full' style={{padding: ".7em .8em", background:"var(--app-color)", boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.155)"}}><AiOutlineSearch /></p></InputAdornment>} />
               <p><Link to="/all" className="underline">View all</Link></p>
             </div>
-            
         </div>
     </div>
   )
